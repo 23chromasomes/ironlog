08420b3768af19b0f62b2046c9ac61b804f46b45
23chromasomes <ihave23dollars@gmail.com>
2026-05-08 20:59:53 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE = 'ironlog-v1';
-const ASSETS = ['/ironlog/', '/ironlog/index.html', '/ironlog/manifest.json'];
+const CACHE = 'ironlog-v2';
+const ASSETS = [
+  '/ironlog/index.html',
+  '/ironlog/manifest.json',
+  '/ironlog/icon.svg'
+];
+
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
@@ -14,6 +19,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/ironlog/index.html')))
   );
 });
